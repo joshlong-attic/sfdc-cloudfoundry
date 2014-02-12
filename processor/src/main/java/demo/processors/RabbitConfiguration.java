396fed636baf8479577a20fa17974e55aec6b924
Josh Long <josh@joshlong.com>
2014-02-11 18:58:03 -0800

    tweaks on UI

--- processor/src/main/java/demo/processors/RabbitConfiguration.java
+++ processor/src/main/java/demo/processors/RabbitConfiguration.java
@@ -3,7 +3,6 @@ package demo.processors;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -32,33 +31,22 @@ class RabbitConfiguration {
     private String routingKey = "sfdc";
 
     private String exchange = "sfdc_exchange";
-
-    @Bean
-    RabbitTemplate fixedReplyQRabbitTemplate(Exchange exchange, @Qualifier(REPLY_Q) Queue replyQueue, ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setExchange(exchange.getName());
-        template.setRoutingKey(this.routingKey);
-        template.setReplyTimeout(-1); // this means that it should wait forever for a reply. TODO is this too dangerous ?
-        template.setReplyQueue(replyQueue);
-        return template;
-    }
+//
+//    @Bean
+//    RabbitTemplate fixedReplyQRabbitTemplate(Exchange exchange, @Qualifier(REPLY_Q) Queue replyQueue, ConnectionFactory connectionFactory) {
+//        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+//        template.setExchange(exchange.getName());
+//        template.setRoutingKey(this.routingKey);
+//        template.setReplyTimeout(-1); // this means that it should wait forever for a reply. TODO is this too dangerous ?
+//        template.setReplyQueue(replyQueue);
+//        return template;
+//    }
 
     @Bean
     AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
         return new RabbitAdmin(connectionFactory);
     }
 
-    // this registers the RabbitTemplate as a SMLC so
-    // that it can do the right thing for any replies coming back
-    @Bean
-    SimpleMessageListenerContainer replyListenerContainer(
-            RabbitTemplate rabbitTemplate, ConnectionFactory connectionFactory) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(REPLY_Q);
-        container.setMessageListener(rabbitTemplate);
-        return container;
-    }
 
     @Bean
     DirectExchange exchange(AmqpAdmin amqpAdmin) {
